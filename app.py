4edc65a5e25cb92796c3a8ce4b4ca3a336dd9bd5
LinHuadong <1513187261@qq.com>
2023-05-12 22:37:48 +0800

    first

---
+++ app.py
@@ -0,0 +1,149 @@
+import streamlit as st
+from streamlit_chat import message
+from inference import chat
+from aip import AipSpeech
+from audio_recorder_streamlit import audio_recorder
+from scipy.io import wavfile
+import numpy as np
+from time import sleep
+
+if 'gen' not in st.session_state:
+    st.session_state['gen'] = []
+
+if 'user' not in st.session_state:
+    st.session_state['user'] = []
+
+if 'temp' not in st.session_state:
+    st.session_state['temp'] = ''
+
+if 'tts_gen' not in st.session_state:
+    st.session_state['tts_gen'] = []
+
+if 'speech_client' not in st.session_state:
+    APP_ID = '32036090'
+    API_KEY = 'Sks9uoXZAfw7NSaA6nTY9eio'
+    SECRET_KEY = 'GYceXIbjFx1HvUA2WSCSfGVQ9a0q54BE'
+    st.session_state['speech_client'] = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+if 'keys' not in st.session_state:
+    st.session_state['keys'] = 0
+
+if 'audio_bytes' not in st.session_state:
+    st.session_state['audio_bytes'] = None
+
+
+def clear_input():
+    st.session_state['temp'] = st.session_state['input']
+    st.session_state['input'] = ''
+
+
+def tts(text, id=None):
+    try:
+        result = st.session_state['speech_client'].synthesis(
+            text,
+            lang='zh',
+            ctp=1,
+            options={
+                'per': 4,
+                'spd': 5,
+            }
+        )
+    except:
+        result = None
+    if isinstance(result, dict):
+        result = None
+    if id == None:
+        return result
+    else:
+        st.session_state['tts_gen'][id] = result
+
+
+def asr(audio_bytes):
+    with open('audio.wav', 'wb') as f:
+        f.write(audio_bytes)
+
+    samplerate, data = wavfile.read('audio.wav')
+    if data.ndim > 1:
+        data = np.mean(data, axis=1, dtype=data.dtype)
+    wavfile.write("audio.wav", samplerate, data)
+
+    with open('audio.wav', 'rb') as f:
+        speech = f.read()
+
+    try:
+        respoense = st.session_state['speech_client'].asr(
+            speech=speech,
+            format='wav',
+            rate=16000
+        )
+        if respoense['err_msg'] != 'success.':
+            return None
+        else:
+            return respoense['result'][0]
+    except:
+        return None
+
+
+st.title('ChatCare')
+
+st.text_input(' ', key='input', on_change=clear_input)
+
+cols_asr = st.columns(9)
+with cols_asr[4]:
+    audio_bytes = audio_recorder(text='', icon_size='2x', sample_rate=16000)
+if st.session_state['audio_bytes'] != audio_bytes:
+    st.session_state['audio_bytes'] = audio_bytes
+    audio_update = True
+else:
+    audio_update = False
+if audio_update:
+    st.session_state['temp'] = asr(audio_bytes=audio_bytes)
+    if st.session_state['temp'] == None:
+        st.error('语音识别发生了某些错误，请重试！', icon='🚨')
+    if st.session_state['temp'] == '':
+        st.warning('说话时间太短了，请重试！', icon='❗')
+
+
+if st.session_state['temp']:
+    with st.spinner('等待回复中...'):
+        output = chat(
+            st.session_state['temp'],
+            user=st.session_state['user'],
+            gen=st.session_state['gen'],
+            temperature=0.7,
+            # repetition_penalty=1
+        )
+
+    if output == 503:
+        # st.info('ChatCare正在加载模型中，请稍等20秒左右重试...', icon='⏳')
+        with st.spinner('⏳ChatCare正在加载模型中，请稍等...'):
+            while chat('') in [503, None]:
+                sleep(5)
+        st.success('模型加载完成，开始聊天吧！', icon='🥳')
+
+    elif output == None:
+        st.error('发生了某些错误，请重试！', icon='🚨')
+
+    else:
+        st.session_state['user'].append(st.session_state['temp'])
+        st.session_state['gen'].append(output)
+        st.session_state['tts_gen'].append(tts(output))
+
+    st.session_state['temp'] = ''
+
+
+if st.session_state['gen']:
+    for i in range(len(st.session_state['gen'])-1, -1, -1):
+
+        message(st.session_state['gen'][i], key=f'gen_{i}')
+        cols_audio_gen = st.columns(2)
+        if st.session_state['tts_gen'][i] == None:
+            cols_audio_gen[0].button(label='转语音失败，请点击重试', type='primary', on_click=tts, kwargs={
+                                     'text': st.session_state['gen'][i], 'id': i}, key=st.session_state['keys'])
+            st.session_state['keys'] += 1
+        else:
+            cols_audio_gen[0].audio(
+                data=st.session_state['tts_gen'][i])
+
+        message(st.session_state['user'][i],
+                is_user=True, key=f'user_{i}')
---
+++ inference.py
@@ -0,0 +1,61 @@
+import requests
+
+
+def preprocess(text):
+    text = text.replace("\n", "\\n").replace("\t", "\\t")
+    return text
+
+
+def postprocess(text):
+    return text.replace("\\n", "\n").replace("\\t", "\t").replace('%20', '  ')
+
+
+def chat(input_text, sample=True, max_length=512, top_p=1, temperature=0.7, user=[], gen=[], top_k=None, repetition_penalty=None, max_time=None):
+    # return input_text
+    if len(user) > 5:
+        user = user[-5:]
+        gen = gen[-5:]
+
+    # print(user)
+    # print(gen)
+
+    context = "\n".join(
+        [f"用户：{user[i]}\n小元：{gen[i]}" for i in range(len(user))])
+    text = f'{context}\n用户：{input_text}\n小元：'
+    text = text.strip()
+    text = preprocess(text)
+
+    # print('input_text:', text)
+
+    model_id = 'llllhd/ChatCare-RLHF'
+    api_token = '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'
+    api_url = f'https://api-inference.huggingface.co/models/{model_id}'
+    headers = {"Authorization": f"Bearer {api_token}"}
+
+    payload = {
+        'inputs': text,
+        'parameters': {
+            'do_sample': sample,
+            'max_length': max_length,
+            'top_p': top_p,
+            'top_k': top_k,
+            'temperature': temperature,
+            'repetition_penalty': repetition_penalty,
+            'max_time': max_time,
+        },
+    }
+    try:
+        response = requests.post(api_url, headers=headers, json=payload)
+        # print('response:', response)
+        if response.status_code == 200:
+            return postprocess(response.json()[0]['generated_text'])
+        elif response.status_code == 503:
+            return 503
+        else:
+            return None
+    except:
+        return None
+
+
+if __name__ == '__main__':
+    print(chat('你好'))
